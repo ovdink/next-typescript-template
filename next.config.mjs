84ae3c373da463a0cd3c5760a3e0aa56d15447bb
KIR <42535133+ovdink@users.noreply.github.com>
2024-03-20 17:09:04 +0300

    updated styled-components settings

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    trailingSlash: false,
+    output: 'standalone',
+    webpack(config) {
+        config.module.rules.push({
+            test: /\.svg$/,
+            issuer: /\.(ts|tsx|js|jsx|md|mdx)$/,
+            use: ['@svgr/webpack'],
+        });
+
+        return config;
+    },
+    compiler: {
+        // Enables the styled-components SWC transform
+        styledComponents: true,
+    },
+};
 
 export default nextConfig;
